7a63004b5e8d3f3eef71237d1f6bb8abb6b84023
Михайлов Евгений <jackmihailov@outlook.com>
2023-10-01 19:26:19 +0300

    Добавьте файлы проекта.

---
+++ 5.5.1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._5._1
+{
+	internal class Program
+	{
+		static string[] SplitText()
+		{
+            Console.WriteLine("Введите предложение для разделения");
+			string EnteredText = Console.ReadLine();
+			string [] EnteredTextArray = EnteredText.Split(new char [] { ' ' });
+            return EnteredTextArray;
+		}
+
+		static void OutputEnteredText(string[] EnteredTextArray)
+		{
+			foreach (string str in EnteredTextArray)
+			{
+				Console.WriteLine(str);
+			}
+			Console.ReadKey();
+			
+		}
+
+		static void Main(string[] args)
+		{
+			OutputEnteredText(SplitText());
+		}
+	}
+}
---
+++ 5.5.2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._5._2
+{
+	internal class Program
+	{
+		static string [] SplitText()
+		{
+			Console.WriteLine("Введите предложение для реверса");
+			string EnteredText = Console.ReadLine();
+			string [] EnteredTextArray = EnteredText.Split(new char[] { ' ' });
+			return EnteredTextArray;
+		}
+		static void ReverseString(string [] EnteredTextArray)
+		{
+			Array.Reverse(EnteredTextArray);
+			foreach (var item in EnteredTextArray)
+			{
+				Console.Write(item + ' ');
+			}
+		}
+		static void Main(string[] args)
+		{
+			ReverseString(SplitText());
+			Console.ReadKey();
+			
+		}
+	}
+}
